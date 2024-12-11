3ae3f10813da14fc2cdd4b87f59eb03e147deb0a
dpielea <dpielea@icloud.com>
2024-12-11 14:58:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //import { OrbitControls } from 'three-orbitcontrols'
 import * as THREE from 'three';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/examples/jsm/controls/OrbitControls.js';
+import { getFresnelMat } from './getFresnelMat.js';
 
     var targetElement = document.getElementById("3D");
     var sexyLady = document.getElementById("sexyContainer");
@@ -20,23 +21,51 @@ import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/exampl
     renderer.setSize(parentWidth, parentHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     targetElement.appendChild(renderer.domElement);
-
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
     // Create a box geometry
     const earthGroup = new THREE.Group();
     earthGroup.rotation.z = -23 * Math.PI /180;
     scene.add(earthGroup)
 
-    const sphere = new THREE.Mesh( new THREE.IcosahedronGeometry(1,12 ), new THREE.MeshLambertMaterial( { 
-        map : new THREE.TextureLoader().load('images/worldmap.jpg')
-        //normalMap : new THREE.TextureLoader().load('images/2k_earth_normal_map.tif')
+    const loader = new THREE.TextureLoader();
+    const Spheregeometry = new THREE.IcosahedronGeometry(1,12);
+    const sphere = new THREE.Mesh( Spheregeometry, new THREE.MeshStandardMaterial( { 
+        map : loader.load('images/earth/8081_earthmap4k.jpg')
     } ) ); 
     earthGroup.add( sphere );
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-    
-    directionalLight.position.set(-0.3,0.3,0.1)
+    const LightsMap = new THREE.MeshBasicMaterial({
+        map : loader.load("images/earth/8081_earthlights4k.jpg"),
+        blending : THREE.AdditiveBlending,
+        opacity : 0.5
+    });
+    const lightMesh = new THREE.Mesh(Spheregeometry, LightsMap);
+    earthGroup.add( lightMesh );
+
+    const cloudMat = new THREE.MeshStandardMaterial({
+        map : loader.load("images/earth/8081_earthhiresclouds4K.jpg"),
+        transparent : true,
+        opacity : 0.5, 
+        blending : THREE.AdditiveBlending,
+    });
+    const cloudMesh = new THREE.Mesh(Spheregeometry, cloudMat);
+    cloudMesh.scale.setScalar(1.005)
+    earthGroup.add(cloudMesh)
+
+    const fresnelMat = getFresnelMat();
+    const glowMesh = new THREE.Mesh(Spheregeometry,fresnelMat)
+    glowMesh.scale.setScalar(1.008)
+    earthGroup.add(glowMesh)
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff,2.3 );
+    directionalLight.position.set(-6,0.5,1.5)
     scene.add(directionalLight);
-   
+
+    /* const lightHelper = new THREE.PointLightHelper(directionalLight)
+    scene.add(lightHelper)
+    const gridHelper = new THREE.GridHelper(200,50)
+    scene.add(gridHelper) */
     // Set camera position
     camera.position.z = 2.4;
     camera.position.x = -0.5;
@@ -46,26 +75,29 @@ import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/exampl
  
         var particle = new THREE.Object3D();
         scene.add(particle);
-        var material = new THREE.MeshPhongMaterial({
+        var Starmaterial = new THREE.MeshPhongMaterial({
             color: 0xffffff,
             shading: THREE.FlatShading
           });
-        var geometry = new THREE.TetrahedronGeometry(0.2, 0); 
-        for (var i = 0; i < 1000; i++) {
-            var mesh = new THREE.Mesh(geometry, material);
+        var Stargeometry = new THREE.TetrahedronGeometry(0.2, 0); 
+        for (var i = 0; i < 2000; i++) {
+            var mesh = new THREE.Mesh(Stargeometry, Starmaterial);
             mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
             mesh.position.multiplyScalar(90 + (Math.random() * 700));
             mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
             particle.add(mesh);
         }
         
-    //const spaceTexture = new THREE.TextureLoader().load('images/2k_stars_milky_way.jpg')
-    //scene.background = spaceTexture
+    const spaceTexture = new THREE.TextureLoader().load('images/2k_stars_milky_way.jpg')
+    scene.background = spaceTexture
 
     // Animation loop
     function animate() {
         requestAnimationFrame(animate);
         sphere.rotation.y += 0.0006;
+        lightMesh.rotation.y += 0.0006;
+        cloudMesh.rotation.y += 0.001;
+        glowMesh.rotation.y += 0.0006;
         renderer.render(scene, camera);
         controls.update()
         particle.rotation.x += 0.0002;
